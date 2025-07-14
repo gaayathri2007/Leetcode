ad29ad164033af101147e99461de7a061ac9173a
gaayathri <gaayathri8383@gmail.com>
2025-07-14 10:52:10 +0530

    Time: 1 ms (5.79%), Space: 9.7 MB (94.02%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.c
@@ -0,0 +1,13 @@
+void sortColors(int* a, int n)
+ {
+    int i, j, c;
+
+    for (i = 0; i < n - 1; i++)
+        for (j = i + 1; j < n; j++)
+            if (a[i] >= a[j])
+             {
+                c = a[i];
+                a[i] = a[j];
+                a[j] = c;
+            }
+}
