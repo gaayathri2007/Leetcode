7fe95e326b55390e07ec2c198a1060533b20df93
gaayathri <gaayathri8383@gmail.com>
2025-07-17 12:30:49 +0530

    Time: 0 ms (100%), Space: 7.9 MB (96.06%) - LeetHub

---
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.c
@@ -0,0 +1,35 @@
+char* reversePrefix(char* s, char ch) 
+{
+
+int n,i,f,j=0,c;
+
+n = strlen(s);
+
+f = 0;
+
+for(i=0;i<n;i++)
+{
+    if(s[i]==ch)
+    {
+        f = 1;
+        break;
+    }
+}
+
+if(f==0)
+return s;
+
+while(j<i)
+{
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+
+    j++;
+    i--;
+}
+
+return s;
+
+
+}
