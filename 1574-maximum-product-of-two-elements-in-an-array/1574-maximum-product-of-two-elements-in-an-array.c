21e64d0841ce2f0f7a5230c9024f12fd87a992b2
gaayathri <gaayathri8383@gmail.com>
2025-07-26 14:15:03 +0530

    Time: 0 ms (100%), Space: 8.7 MB (9.17%) - LeetHub

--- 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.c
+++ 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.c
@@ -1,15 +1,14 @@
+int compare(const void* , const void*);
 int maxProduct(int* a, int n) 
 {
 int i,j,c;
 
-for(i=0;i<n-1;i++)
-for(j=i+1;j<n;j++)
-if(a[i]>a[j])
-{
-c = a[i];
-a[i] = a[j];
-a[j] = c;
-}
+qsort(a,n,sizeof(int),compare);
 
 return (a[n-1]-1)*(a[n-2]-1);
+}
+
+int compare(const void* a,const void* b)
+{
+return (*(int*)a - *(int*)b);
 }
