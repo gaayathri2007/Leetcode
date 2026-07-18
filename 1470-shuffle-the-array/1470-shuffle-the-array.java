a9cfb022bef64f9ca65133bee7cf926fc81111ee
gaayathri <gaayathri8383@gmail.com>
2026-07-18 23:36:33 +0530

    Time: 8 ms (0.72%), Space: 46.4 MB (62.42%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -4,19 +4,6 @@ class Solution {
 
         List<Integer> l = new ArrayList<>();
 
-        // List<Integer> fl = new ArrayList<>();
-        // List<Integer> sl = new ArrayList<>();
-
-
-        // for(int i=0; i<N/2; i++)
-        // fl.add(nums[i]);
-
-        // for(int i=N/2; i<N; i++)
-        // sl.add(nums[i]);
-
-        // System.out.println(fl);  
-        // System.out.println(sl);  
-
         int j = n;
         for(int i=0; i<N/2; i++){
             l.add(nums[i]);
