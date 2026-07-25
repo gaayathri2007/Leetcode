152e3d7c19495e105f94185be22e54ad5e338b99
gaayathri <gaayathri8383@gmail.com>
2026-07-25 12:28:56 +0530

    Time: 0 ms (100%), Space: 45.1 MB (53.96%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.java
+++ 3151-special-array-i/3151-special-array-i.java
@@ -3,9 +3,11 @@ class Solution {
         int N = nums.length;
 
         for(int i=0; i<N-1; i++){
-            if(nums[i] % 2 == 0 && nums[i+1] % 2 == 0)
-            return false;
-            else if(nums[i] % 2 != 0 && nums[i+1] % 2 != 0)
+            // if(nums[i] % 2 == 0 && nums[i+1] % 2 == 0)
+            // return false;
+            // else if(nums[i] % 2 != 0 && nums[i+1] % 2 != 0)
+            // return false;
+            if((nums[i] + nums[i+1]) % 2 == 0)
             return false;
         }
         return true;
